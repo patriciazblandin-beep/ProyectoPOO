60b177dbb539adb3b1894de8d69e69c40406aaf3
PatriciaB <patriciazblandin@gmail.com>
2025-11-16 14:55:15 -0600

    Agrego Dockerfile y actualizo requerimientos, main.py

--- LevantarAPI/main.py
+++ LevantarAPI/main.py
@@ -1,6 +1,6 @@
+import uvicorn 
 from typing import Union
 from fastapi import FastAPI
-
 
 app = FastAPI()
 
@@ -13,3 +13,6 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info")
