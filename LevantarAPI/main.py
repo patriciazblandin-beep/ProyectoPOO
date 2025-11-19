6f7a6a77671144dd8fb9decee62e4a08b4967ad2
PatriciaB <patriciazblandin@gmail.com>
2025-11-18 18:46:35 -0600

    se actualizo main

--- LevantarAPI/main.py
+++ LevantarAPI/main.py
@@ -1,19 +1,20 @@
 from fastapi import FastAPI
-# ----------------------------------------------------------------------
-# 1. Importación EXPLICITA del objeto 'router' de cada archivo
-# Esto es más robusto que "from routes import usuarios"
+
 # ----------------------------------------------------------------------
 from routes.usuarios import router as usuarios_router
-from routes.playlists import router as playlists_router
+from routes.playlists import router as playlists_router 
+from routes.artistas  import router as artista_router
+
 
 # Inicialización de la aplicación FastAPI
 app = FastAPI()
 
-# Incluir los routers
 app.include_router(usuarios_router, prefix="/usuarios")
 app.include_router(playlists_router, prefix="/playlists")
+app.include_router(artista_router, prefix="/artistas")
 
-# Ruta raíz simple para verificar que la API está viva
+
+# Ruta raíz simple para verificar que la API está viv
 @app.get("/")
 def read_root():
     return {"message": "API de música activa."}
