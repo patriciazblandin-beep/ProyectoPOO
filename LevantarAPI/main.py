00689fc8b847a45ea9a3c38c90acdad5fe287539
PatriciaB <patriciazblandin@gmail.com>
2025-11-19 20:55:10 -0600

    actualizo main

--- LevantarAPI/main.py
+++ LevantarAPI/main.py
@@ -3,8 +3,9 @@ from fastapi import FastAPI
 # ----------------------------------------------------------------------
 from routes.usuarios import router as usuarios_router
 from routes import playlists
-
-from routes.artistas  import router as artista_router
+from routes import artistas
+from routes import albumes
+from routes import canciones
 
 
 # Inicialización de la aplicación FastAPI
@@ -12,11 +13,13 @@ app = FastAPI()
 
 app.include_router(usuarios_router, prefix="/usuarios")
 app.include_router(playlists.router, prefix="/playlists")
+app.include_router(artistas.router, prefix="/artistas")
+app.include_router(albumes.router, prefix="/albumes")
+app.include_router(canciones.router, prefix="/canciones")
 
-app.include_router(artista_router, prefix="/artistas")
 
 
 # Ruta raíz simple para verificar que la API está viv
 @app.get("/")
 def read_root():
-    return {"message": "API de música activa."}
+    return {"message": "API de música activa 1."}  
