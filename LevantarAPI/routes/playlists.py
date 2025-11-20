7b545877a9b7a0d1d18c041483b47677c29c139e
PatriciaB <patriciazblandin@gmail.com>
2025-11-19 21:09:19 -0600

    se actualizo routes

--- LevantarAPI/routes/playlists.py
+++ LevantarAPI/routes/playlists.py
@@ -7,37 +7,33 @@ router = APIRouter(
     tags=["Playlists"],
 )
 
-# ----------------------------------------------------------------------
 # 1. POST: Crear Nueva Playlist
-# ----------------------------------------------------------------------
 @router.post("/", response_model=PlaylistOut, status_code=status.HTTP_201_CREATED, summary="Crea una nueva playlist")
 async def create_playlist_route(playlist: PlaylistIn):
     return await playlist_controller.create_playlist(playlist)
 
-# ----------------------------------------------------------------------
-# 2. GET: Obtener TODAS las Playlists
-# ----------------------------------------------------------------------
+# Obtener TODAS las Playlists
+
 @router.get("/", response_model=List[PlaylistOut], summary="Obtiene todas las playlists")
 async def get_all_playlists_route():
     return await playlist_controller.get_all_playlists()
 
-# ----------------------------------------------------------------------
-# 3. GET: Obtener Playlist por ID
-# ----------------------------------------------------------------------
-@router.get("/{id_playlists}", response_model=PlaylistOut, summary="Obtiene una playlist por su ID")
+
+# Obtener Playlist por Id
+
+@router.get("/{id_playlists}", response_model=PlaylistOut, summary="obtiene una playlists por su ID")
 async def get_playlist_by_id_route(id_playlists: int):
     return await playlist_controller.get_one_playlist(id_playlists)
 
-# ----------------------------------------------------------------------
-# 4. PUT: Actualizar Playlist por ID
-# ----------------------------------------------------------------------
+
+#  Actualizar Playlist por ID
+
 @router.put("/{id_playlists}", response_model=PlaylistOut, summary="Actualiza una playlist existente")
 async def update_playlist_route(id_playlists: int, playlist: PlaylistUpdate):
     return await playlist_controller.update_playlist(id_playlists, playlist)
 
-# ----------------------------------------------------------------------
 # 5. DELETE: Eliminar Playlist por ID
-# ----------------------------------------------------------------------
+
 @router.delete("/{id_playlists}", status_code=status.HTTP_204_NO_CONTENT, summary="Elimina una playlist por su ID")
 async def delete_playlist_route(id_playlists: int):
     deleted = await playlist_controller.delete_playlist(id_playlists)
